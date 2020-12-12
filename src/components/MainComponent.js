a1590441b1b4ce9e70c3eb1950c4eb1b68ecedd1
shahaj90 <gshahaj@gmail.com>
2020-12-12 12:38:41 +0600

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,15 +4,20 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from "./MenuComponent";
 import Home from './HomeComponent';
-import DishDetail from "./DishdetailComponent";
+import Contact from './ContactComponent';
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -24,6 +29,9 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
         />
       );
     }
@@ -34,6 +42,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
